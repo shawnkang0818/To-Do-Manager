d5b891479618eadbe280c743cafbe0a02ab3ea48
Shawn Kang <shawnkang0818@gmail.com>
2024-03-04 03:59:57 -0500

    add tasks page and CSS

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
   path('', views.home, name='home'),
-  path('todotask/', views.todo_task, name='todo_task')
+  path('todotask/', views.todo_task, name='todo_task'),
+  path('tasks/', views.task_index, name='task-index'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+# Add the Task class & list and view function below the imports
+class Task:  
+  def __init__(self, title, priority, description):
+    self.title = title
+    self.priority = priority
+    self.description = description
+
+tasks = [
+  Task('exercise', 'high', '5 set of push up'),
+  Task('exercise', 'low', '2 set of push up'),
+  Task('exercise', 'mid', '3 set of push up'),
+]
+
 # Create your views here.
 # home view
 def home(request):
@@ -8,3 +21,7 @@ def home(request):
 
 def todo_task(request):
   return render(request, 'todotask.html')
+
+# Add new view
+def task_index(request):
+  return render(request, 'tasks/index.html', {'tasks': tasks})
